e8b68188f160dbd68e112765e8c152cd29650838
Pratik Singh <61048532+imsine0@users.noreply.github.com>
2021-07-02 14:27:26 +0530

    Update settings.py

--- jcv2/settings.py
+++ jcv2/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'maryu^zyezw^=p09wnz(nf3o(-$vhmhz71s$(4cu(yj_93w&jn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
